b7e611f4b56b1e02b770647b88069ae6af9f9b0d
Piyusha Patel <48045515+arya0618@users.noreply.github.com>
2025-12-27 12:23:34 +0530

    Update health-check.controller.ts

--- task-service/src/module/health-check/health-check.controller.ts
+++ task-service/src/module/health-check/health-check.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { constants } from '../../helpers/constants';
+
 @ApiTags(constants.HEALTH_CONTROLLER)
 @Controller(constants.HEALTH_CONTROLLER)
 export class HealthCheckController {
+  
   @Get('/health')
   healthStatus(): object {
     return { status: 200, message: 'Task-service healthcheck OK ! ....' }
   }
 }
+
